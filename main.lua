8e2a930585c2b96373ee175023305f0c383cb4e3
alexquio <100390429+alexquio@users.noreply.github.com>
2022-12-28 12:40:58 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -17,7 +17,13 @@ function love.draw()
   scenes[scene]:draw()
 end
 
+function love.mousepressed(x, y, button, istouch, presses)
+  scenes[scene]:mousepressed(x, y, button, istouch, presses)
+end
+
 function love.keypressed(key)
+  scenes[scene]:keypressed(key)
+  
   -- if you dont need exit game when escape key pressed, just delete this if statement
   if key == 'escape' then
     love.event.quit()
